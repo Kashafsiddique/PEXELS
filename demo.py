8c966f0df9373cc79cdaa703c1fdbfddd2992c30
Kashafsiddique <125047436+Kashafsiddique@users.noreply.github.com>
2023-02-12 10:55:10 +0500

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,72 @@
+#variable
+x = "Aiza"
+y = 10
+print(x)
+print(y)
+
+#comment
+print("Hello, World!") #This is my First python code!
+
+#strings
+a = "Bisma"
+print(a)
+
+ #conditional statement
+#if statement
+a = 13
+b = 20
+if b > a:
+  print("b is greater than a")
+
+#if_else
+X = 33
+Y = 33
+if X > Y:
+  print("X is greater than Y ")
+elif X== Y:
+  print("X and Y are equal")
+
+#python Array
+cars = ["Mango", "Apple", "Banana"]
+
+#List
+List1 = ["apple", "banana", "cherry"]
+print(List1)
+
+#Tuple
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+#Set
+SetA= {"apple", "banana", "cherry", "apple"}
+
+print(SetA)
+
+#Dictionary
+Dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(Dict)
+
+#replace function
+c=("I LOVE pakistan.i blong to pakistan")
+x=c.replace("pakistan","India")
+print(x)
+
+#Data types
+a=71 #integer
+b=1.2 #float
+c="hello" #string
+#TYPE CASTING
+a=int(2.8) #output will be 2
+b=float("4.2") #output will be 4.2
+c=str("hello") #output will be hello
+
+# string slicing
+#start
+b="hello , world!"
+print(b[2:5])
+
+
